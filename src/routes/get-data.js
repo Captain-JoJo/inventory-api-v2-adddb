90a51de26091691b1e9a537f4b276d5fd3fcb139
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-12 16:05:48 -0600

    added in cors info

--- src/routes/get-data.js
+++ src/routes/get-data.js
@@ -5,11 +5,43 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const inventorySchema = require('../db/models/item')
 
-app.use(cors())
-app.use("/", router)
+// const corsOptions = {
+//     origin: 'https://captain-jojo.github.io',
+// }
+
+// function corsRouter(app) {
+//     app.use(cors())
+//     app.use("/", router)
+//     app.get('/getData', cors(), function (req, res, next) {
+//         res.json({msg: 'This is CORS-enabled for a Single Route'})
+//     })
+// }
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
 
 //Single router
-router.route("/getData").get(function(req, res){
+// function corsRouter() {
+//     app.get('getData', cors(), function (req, res, next) {
+//         const Item = mongoose.model("Item", inventorySchema)
+//         Item.find({}, function(err, result) {
+//             if(err) {
+//                 res.send(err)
+//             } else {
+//                 res.send(result)
+//             }
+//         })
+//       })
+// }
+
+router.route("/getData").get(function(req, res,next){
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // next();
     const Item = mongoose.model("Item", inventorySchema)
     Item.find({}, function(err, result) {
         if(err) {
@@ -19,5 +51,7 @@ router.route("/getData").get(function(req, res){
         }
     })
 })
+
+//module.exports = {corsRouter}
 
 module.exports = router
