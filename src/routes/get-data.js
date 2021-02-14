a82e270eed122a4dead5489bff5dd2b8ed55f874
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-13 18:11:48 -0600

    updated routes

--- src/routes/get-data.js
+++ src/routes/get-data.js
@@ -5,6 +5,40 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const inventorySchema = require('../db/models/item')
 
+
+
+router.route("/getData").get(function(req, res){
+    res.header("Access-Control-Allow-Origin", "https://captain-jojo.github.io");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    const Item = mongoose.model("Item", inventorySchema)
+    Item.find({}, function(err, result) {
+        if(err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+router.route("/deleteAll").get(function(req, res){
+    res.header("Access-Control-Allow-Origin", "https://captain-jojo.github.io");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    const Item = mongoose.model("Item", inventorySchema)
+    Item.deleteMany({}, function(err, result) {
+        if(err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+module.exports = router
+
+
+
+
+
 // const corsOptions = {
 //     origin: 'https://captain-jojo.github.io',
 // }
@@ -38,18 +72,4 @@ const inventorySchema = require('../db/models/item')
 //       })
 // }
 
-router.route("/getData").get(function(req, res){
-    res.header("Access-Control-Allow-Origin", "https://captain-jojo.github.io");
-    const Item = mongoose.model("Item", inventorySchema)
-    Item.find({}, function(err, result) {
-        if(err) {
-            res.send(err)
-        } else {
-            res.send(result)
-        }
-    })
-})
-
 //module.exports = {corsRouter}
-
-module.exports = router
