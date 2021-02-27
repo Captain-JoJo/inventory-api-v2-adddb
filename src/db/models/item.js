28c6f88d88a37d683a46734c2080540872d44bb2
Tessa Seiders <tessa.seiders@gmail.com>
2021-02-27 12:59:51 -0600

    updated to use express instead

--- src/db/models/item.js
+++ src/db/models/item.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose")
 const db = require('../config')
 mongoose.connect(db.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
-//mongoose.connect('mongodb+srv://${MONGO_USER}:${MONOG_PASS}@inventorycluster0.4qlqj.mongodb.net/inventoryDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-//mongoURI: 'mongodb+srv://${MONGO_USER}:${MONOG_PASS}@inventorycluster0.4qlqj.mongodb.net/inventoryDB?retryWrites=true&w=majority'
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/inventoryDB', {useNewUrlParser: true, useUnifiedTopology: true})
 
 //MongoDB schema
 const inventorySchema = new mongoose.Schema({
