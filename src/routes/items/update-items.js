6d36276e7e03a7515fc9efcac251a14a12ed3078
Tessa Seiders <tessa.seiders@gmail.com>
2021-03-19 09:07:29 -0500

    updated to use the update method

--- src/routes/items/update-items.js
+++ src/routes/items/update-items.js
@@ -1,32 +1,41 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require("mongoose")
-const inventorySchema = require('../../db/models/item')
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const inventorySchema = require("../../db/models/item");
+const ObjectId = require("mongodb").ObjectID;
 
-const Item = mongoose.model("Item", inventorySchema)
+const Item = mongoose.model("Item", inventorySchema);
 
 //Updates many data items
-router.route("/updateMany").put(function(req, res){
-    Item.updateMany({}, function(err, result) {
-        if(!err) {
-            res.send(result)
-            console.log('Updated MANY items', result);
-        } else {
-            res.status(500).send(err)
-        }
-    })
-})
+router.route("/updateMany").put(function (req, res) {
+  Item.updateMany({}, function (err, result) {
+    if (!err) {
+      res.send(result);
+      console.log("Updated MANY items", result);
+    } else {
+      res.status(500).send(err);
+    }
+  });
+});
 
 //Updates one item
-router.route("/updateOne").put(function(req, res){
-    Item.updateOne({}, function(err, result) {
-        if(!err) {
-            res.send(result)
-            console.log('Updated ONLY 1 item', result);
-        } else {
-            res.status(500).send(err)
-        }
-    })
-})
+router.route("/updateOne/:id").put(function (req, res) {
+  const _id = req.params.id;
 
-module.exports = router
+  Item.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: req.body },
+    function (err, result) {
+      if (!err) {
+        console.log("this is the ID I want to update", _id);
+        console.log("all params", req.params);
+        res.send(result);
+        console.log("Updated ONLY 1 item", result);
+      } else {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+module.exports = router;
